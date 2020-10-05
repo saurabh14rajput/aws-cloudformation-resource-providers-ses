2a57c82b0e77a35d359c8832f511fdaffa37784c
Saurabh Singh <singhrdo@amazon.com>
2020-10-05 23:19:31 +0000

    Init resource handlers

---
+++ aws-iot-mydummyresource/src/main/java/software/amazon/iot/mydummyresource/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.iot.mydummyresource;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-iot-mydummyresource.json");
+    }
+}
